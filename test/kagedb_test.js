d96e020af52793b37645f1ecc55e2745f802dabf
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2012-05-03 16:55:05 +0900

    Add dump function

--- test/kagedb_test.js
+++ test/kagedb_test.js
@@ -58,3 +58,61 @@ test("cmp", function () {
     strictEqual(kageDB.cmp(2, 1), 1);
     strictEqual(kageDB.cmp(1, 2), -1);
 });
+
+module("kagedb_dump_test", {
+    setup: function () {
+        function open() {
+            var req = kageDB.open("MyDB");
+            req.onupgradeneeded = function (event) {
+                var db = event.target.result;
+                var store = db.createObjectStore("MyStore", { autoIncrement: true });
+                store.createIndex("name", "name", { unique: true });
+                var store2 = db.createObjectStore("MyStore2", { autoIncrement: true });
+                store2.createIndex("product", "product", { unique: true });
+            };
+            req.onsuccess = function (event) {
+                var db = event.target.result;
+                var tx = db.transaction(["MyStore", "MyStore2"], IDBTransaction.READ_WRITE);
+                var store = tx.objectStore("MyStore");
+                var req = store.bulkAdd([
+                    { name: "aaa", age: 20 },
+                    { name: "bbb", age: 30 },
+                    { name: "ccc", age: 40 }
+                ]);
+                req.onsuccess = function () {
+                    var store2 = tx.objectStore("MyStore2");
+                    var req = store2.bulkAdd([
+                        { product: "ddd", category: 20 },
+                        { product: "eee", category: 30 },
+                        { product: "fff", category: 40 }
+                    ]);
+                    req.onsuccess = function () {
+                        start();
+                    };
+                };
+            };
+        }
+        stop();
+        var kageDB = new KageDB();
+        var req = kageDB.deleteDatabase("MyDB");
+        req.onsuccess = req.onerror = open;
+    }
+});
+
+asyncTest("dump", function () {
+    var kageDB = new KageDB();
+    var req = kageDB.dump("MyDB", ["MyStore", "MyStore2"]);
+    req.onsuccess = function (event) {
+        var results = event.target.result;
+        ok(results.length, 2);
+        deepEqual(results[0], [
+            { name: "aaa", age: 20 },
+            { name: "bbb", age: 30 },
+            { name: "ccc", age: 40 }]);
+        deepEqual(results[1], [
+            { product: "ddd", category: 20 },
+            { product: "eee", category: 30 },
+            { product: "fff", category: 40 }]);
+        start();
+    };
+});
